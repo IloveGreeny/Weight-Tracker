edf7df16e8df1ddac11e97a24bea3be82dd088e0
IloveGreeny <greenxd23@gmail.com>
2023-09-20 22:37:49 +0400

    msg

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import WeightTrackerHeader from "../src/components/WeightTrackerHeader/WeightTrackerHeader.vue";
+import WeightTrackerForm from "../src/components/WeightTrackerForm/WeightTrackerForm.vue";
 </script>
 
 <template>
   <WeightTrackerHeader/>
+  <WeightTrackerForm/>
 </template>
 
 <style scoped>
---
+++ src/components/WeightTrackerForm/WeightTrackerForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from "vue";
+
+
+</script>
+
+
+<template>
+<form class="border-amber-100 bg-amber-700">
+<h1>Add Data</h1>
+  <p>Enter your weight</p>
+  <input type="text" placeholder="Enter your weight(kg)"/>
+  <input type="text" placeholder="Enter your height(cm)"/>
+  <button @click="console.log(1)">ADD</button>
+</form>
+</template>
+
+
+
+<style scoped>
+
+</style>
