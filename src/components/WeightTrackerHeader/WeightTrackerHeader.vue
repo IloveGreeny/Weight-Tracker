6e90d6b40366645c6ef80240c109d8b4e58421bd
IloveGreeny <greenxd23@gmail.com>
2023-09-20 21:42:44 +0400

    msg

---
+++ src/components/WeightTrackerHeader/WeightTrackerHeader.vue
@@ -0,0 +1,30 @@
+
+<script setup>
+
+</script>
+
+
+<template>
+
+  <div class="header">
+    <header>
+      <nav>
+        <h1>
+          Weight Tracker
+        </h1>
+      </nav>
+    </header>
+  </div>
+
+
+</template>
+
+
+
+<style scoped>
+
+.header {
+  background-color: cornflowerblue;
+}
+
+</style>
