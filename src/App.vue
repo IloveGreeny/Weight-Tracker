c2d6649796ae13d6a549de2b1e2892d1279a3e6a
IloveGreeny <greenxd23@gmail.com>
2023-09-21 15:46:13 +0400

    msg

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import WeightTrackerHeader from "../src/components/WeightTrackerHeader/WeightTrackerHeader.vue";
 import WeightTrackerForm from "../src/components/WeightTrackerForm/WeightTrackerForm.vue";
+
+import {RouterLink,RouterView} from "vue-router"
 </script>
 
 <template>
   <WeightTrackerHeader/>
   <WeightTrackerForm/>
+
+  <RouterLink to="/bmi">Go to BMI calculations</RouterLink>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
---
+++ src/views/WeightBMIView/WeightBMIView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import WeightBMI from "../../components/WeightBMI/WeightBMI.vue";
+
+</script>
+
+<template>
+
+  <WeightBMI/>
+
+</template>
+
+
+<style scoped>
+
+</style>
