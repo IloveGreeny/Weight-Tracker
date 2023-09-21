e68ad9785caf25890ce99eccf680f9b3ea86485a
IloveGreeny <greenxd23@gmail.com>
2023-09-21 15:24:51 +0400

    msg

--- src/components/WeightTrackerForm/WeightTrackerForm.vue
+++ src/components/WeightTrackerForm/WeightTrackerForm.vue
@@ -1,8 +1,21 @@
 <script setup>
 import {ref} from "vue";
 
-const weight = ref("60 kg");
-const height = ref("174 cm");
+let weight = ref("");
+let height = ref("");
+
+
+
+function addWeight() {
+  event.preventDefault();
+  let val = document.getElementById("weight").value;
+  weight.value = val
+  console.log(weight);
+  let val2 = document.getElementById("height").value;
+  height.value= val2;
+  console.log(height);
+}
+
 
 </script>
 
@@ -11,9 +24,9 @@ const height = ref("174 cm");
 <form class="border-amber-100 bg-amber-700">
 <h1>Add Data</h1>
   <p>Enter your weight</p>
-  <input type="text" placeholder="Enter your weight(kg)"/>
-  <input type="text" placeholder="Enter your height(cm)"/>
-  <button @click="console.log(1)">ADD</button>
+  <input type="text" id="weight" placeholder="Enter your weight(kg)"/>
+  <input type="text" id="height" placeholder="Enter your height(cm)"/>
+  <button  @click="addWeight()">ADD</button>
   <div>
     <h1>Facts</h1>
     <div>
