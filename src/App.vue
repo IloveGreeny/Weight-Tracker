febb392edcb5466fbf3ce4bbe5aac94a27d259ac
IloveGreeny <greenxd23@gmail.com>
2023-09-23 01:55:53 +0400

    done project

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
 import WeightTrackerHeader from "../src/components/WeightTrackerHeader/WeightTrackerHeader.vue";
 import WeightTrackerForm from "../src/components/WeightTrackerForm/WeightTrackerForm.vue";
 
-import {RouterLink,RouterView} from "vue-router"
 </script>
 
 <template>
   <WeightTrackerHeader/>
   <WeightTrackerForm/>
-
 </template>
 
 <style scoped>
